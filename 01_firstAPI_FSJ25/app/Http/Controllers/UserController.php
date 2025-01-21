e8514fe4b8d79a624dfaf8e0d2e0776fcd11ddf3
KevArevalo026 <kevinarepalacios123@gmail.com>
2025-01-20 20:29:11 -0600

    Creando funcionalidad parcial de metodo LOGIN es UserController

--- 01_firstAPI_FSJ25/app/Http/Controllers/UserController.php
+++ 01_firstAPI_FSJ25/app/Http/Controllers/UserController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
 
+    //Funcion para registrar un User
     public function register(Request $request)
     {
         try {
@@ -30,6 +32,44 @@ class UserController extends Controller
                 'data' => $user
             ], 201);
         } catch (Exception $error) {
+            return response()->json([
+                'error' => $error->getMessage()
+            ], 400);
+        }
+    }
+
+    //Funcion para login de un usuario
+    public function login(Request $request) {
+
+        try{
+            //Validate de los campos
+            $request->validate([
+                'email' => 'required|string|email|max:255',
+                'password' => 'required|string|min:8'
+            ]);
+
+            //Verificando usuario ("Attempt" es usado para validar datos, devolviendo TRUE o FALSE)
+            $credentials = $request->only('email', 'password');
+
+            if(!Auth::attempt($credentials)) {
+                throw new Exception('Invalid credentials');
+            }
+
+            //Obtencion de los datos ingresados 
+            $user = $request->user();
+
+            //Crear token
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            //Respuesta si se da exitoso el LOGIN
+            return response()->json([
+                'message' => 'User logged successfuly',
+                'user' => $user,
+                'token' => $token,
+                'token_type' => 'Bearer'
+            ]);
+
+        }catch(Exception $error){
             return response()->json([
                 'error' => $error->getMessage()
             ], 400);
--- 01_firstAPI_FSJ25/routes/api.php
+++ 01_firstAPI_FSJ25/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController; //Controlador Task
+use App\Http\Controllers\UserController; //Controlador User
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
