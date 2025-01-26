1de4e02d088f2580bd8bef8ab4bcaf7fec6c14c0
KevArevalo026 <kevinarepalacios123@gmail.com>
2025-01-25 19:01:48 -0600

    Finalizando ejercicios de Query Builder en Laravel, generando test de cada uno porbados como ROUTES de API y usando Thunder Client

--- 02_Query_Builder/app/Http/Controllers/QbuilderController.php
+++ 02_Query_Builder/app/Http/Controllers/QbuilderController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB; //Facada para empezar a usar Query Builder de
 
 class QbuilderController extends Controller
 {
-    //Metodo para mostrar todos los datos
+    //Metodo para mostrar todos los datos (Creado para ver correctamente las inserciones)
     public function index()
     {
         $users = DB::table('usuarios')->get();
@@ -25,7 +25,7 @@ class QbuilderController extends Controller
     {
 
         DB::table('usuarios')->delete();
-        DB::statement('ALTER TABLE usuarios AUTO_INCREMENT = 1');
+        DB::statement('ALTER TABLE usuarios AUTO_INCREMENT = 1'); //Reinicia el AUTO_INCREMENT a 1 en los usuarios
 
         return response()->json([
             'message' => 'Records delete successfully'
@@ -35,7 +35,6 @@ class QbuilderController extends Controller
     // 1) Insercion de al menos 5 registros en tablas "users"
     public function insertUsuarios()
     {
-
         DB::table('usuarios')->insert([
             [
                 'nombre' => 'kevin arevalo',
@@ -43,7 +42,7 @@ class QbuilderController extends Controller
                 'telefono' => '+503 75489541'
             ],
             [
-                'nombre' => 'valentina cuellar',
+                'nombre' => 'renata cuellar',
                 'correo' => 'valentina@example.com',
                 'telefono' => '+503 56487541'
             ],
@@ -53,7 +52,7 @@ class QbuilderController extends Controller
                 'telefono' => '+503 75489541'
             ],
             [
-                'nombre' => 'pepito fuentes',
+                'nombre' => 'reynaldo fuentes',
                 'correo' => 'pepito@example.com',
                 'telefono' => '+503 45789744'
             ],
@@ -69,6 +68,7 @@ class QbuilderController extends Controller
             ]
         ]);
 
+        //Se hace un SELECT * FROM usuarios para visualizar que se han ingresado las inserciones y se retorna la respuesta de exito status:201
         $users = DB::table('usuarios')->get();
 
         return response()->json([
@@ -80,7 +80,6 @@ class QbuilderController extends Controller
     // 1) Insercion de al menos 5 registros en tablas "pedidos"
     public function insertPedidos()
     {
-
         //Pedidos generados con tematica de compra de tecnologia
         DB::table('pedidos')->insert([
             [
@@ -99,7 +98,7 @@ class QbuilderController extends Controller
                 'producto' => 'Audifonos skull Candy',
                 'cantidad' => 2,
                 'total' => 50,
-                'usuario_id' => 2
+                'usuario_id' => 5
             ],
             [
                 'producto' => 'PlayStation 5',
@@ -145,6 +144,7 @@ class QbuilderController extends Controller
             ]
         ]);
 
+        //Al igual que la insercion de usuarios, se hace un SELECT * FROM pedidos y se visualizan los registros insertados
         $pedidos = DB::table('pedidos')->get();
 
         return response()->json([
@@ -153,4 +153,152 @@ class QbuilderController extends Controller
         ], 201);
     }
 
+    // 2) Recupera todos los pedidos asociados al usuario con ID 2
+    public function getUser2()
+    {
+
+        $user2 = DB::table('pedidos')
+            ->where('usuario_id', 2) //Se especifica con ->where el id a recuperar
+            ->get();                 //Se obtienen los registros para lanzar respuesta
+
+        return response()->json([
+            'message' => 'Orders from user 2 selected',
+            'data' => $user2
+        ]);
+    }
+
+    // 3) Obtén la información detallada de los pedidos, incluyendo el nombre y correo electrónico de los usuarios
+    public function detailOrder()
+    {
+
+        $detailOrder = DB::table('pedidos')                               //Tabla donde empezara la consulta
+            ->join('usuarios', 'pedidos.usuario_id', '=', 'usuarios.id')  //Se hace JOIN con 'usuarios', donde 'usuario_id' de tabla 'pedidos' debe coincidir con 'id' de tabla 'usuarios'
+            //ADICIONAL: Obligatorio referencia el campo con el nombre completo de la tabla, por regla de Laravel
+            ->select(           //Se hace un SELECT de los campos a aparecer
+                'pedidos.*',        //Se selecciona 'todo' de tabla pedidos 
+                'usuarios.nombre',   //Seleccion de 'nombre' de usuarios 
+                'usuarios.correo'    //Seleccion de 'correo; de usuarios 
+            )
+            ->get();  //Obtencion de la consulta
+
+        //Retorno de respuesta de exito
+        return response()->json([
+            'message' => 'Data selected:',
+            'data' => $detailOrder
+        ]);
+    }
+
+    // 4) Recupera todos los pedidos cuyo total esté en el rango de $100 a $250
+    public function rangeOrders()
+    {
+
+        $range = DB::table('pedidos')
+            ->whereBetween('total', [100, 250]) //Laravel usa 'whereBetween' para usar clausula BETWEEN que compara entre rango de numeros
+            ->get();
+
+        //Respuesta de exito
+        return response()->json([
+            'message' => 'Data selected with specific total range',
+            'data' => $range
+        ]);
+    }
+
+    // 5) Encuentra todos los usuarios cuyos nombres comiencen con la letra "R"
+    public function user_with_r()
+    {
+
+        $userR = DB::table('usuarios')
+            ->whereLike('nombre', 'R%', caseSensitive: false) //Laravel posee 'whereLike' para usar clausula LIKE
+            ->get();
+
+        //Respuesta de exito
+        return response()->json([
+            'message' => 'Users sorted succesfully',
+            'data' => $userR
+        ]);
+    }
+
+    // 6) Calcula el total de registros en la tabla de pedidos para el usuario con ID 5
+    public function totalUser5()
+    {
+
+        $user5 = DB::table('pedidos')
+            ->where('usuario_id', 5) //Primero se crea la condicion para contar los pedidos de usuario con ID 5
+            ->count();               //Se ejecuta count() para contar reistros que cumplan dicha condicion
+
+        //Respuesta de exito
+        return response()->json([
+            'message' => 'User 5 total orders sorted succesfully',
+            'data' => 'There are ' . $user5 . ' orders with user 5'
+        ]);
+    }
+
+    // 7) Recupera todos los pedidos junto con la información de los usuarios, ordenándolos de forma descendente según el total del pedido.
+    public function descOrders()
+    {
+
+        $descOrders = DB::table('pedidos')
+            ->join('usuarios', 'pedidos.usuario_id', '=', 'usuarios.id') //Se genera un JOIN con todos los campos de 'usuarios' y 'pedidos'
+            ->select('pedidos.*', 'usuarios.*')
+            ->orderBy('pedidos.total', 'desc') //Laravel maneja clausula 'orderBy' de SQL especificando columna a ordenar y si es ASC o DESC
+            ->get();
+
+        //Respuesta de exito
+        return response()->json([
+            'message' => 'Orders sorted by descendant order',
+            'data' => $descOrders
+        ]);
+    }
+
+    // 8) Obtén la suma total del campo "total" en la tabla de pedidos
+    public function sumTotal()
+    {
+
+        $sumTotal = DB::table('pedidos')->sum('total'); //Laravel maneja la suma de campos numericos directamnte con 'sum()' 
+
+        //Respuesta de exito
+        return response()->json([
+            'Total amount' => 'The total is: $' . $sumTotal
+        ]);
+    }
+
+    // 9) Encuentra el pedido más económico, junto con el nombre del usuario asociado
+    public function minTotal()
+    {
+
+        $min = DB::table('pedidos')->min('total');
+
+        $cheapestOrder = DB::table('pedidos')
+            ->join('usuarios', 'pedidos.usuario_id', '=', 'usuarios.id')
+            ->select('pedidos.*', 'usuarios.nombre')
+            ->where('pedidos.total', $min)
+            ->get();
+
+        //Respuesta de exito
+        return response()->json([
+            'message' => 'Cheapest order selected: ',
+            'Cheapest order' => $cheapestOrder
+        ]);
+    }
+
+    // 10) Obtén el producto, la cantidad y el total de cada pedido, agrupándolos por usuario
+    public function orders()
+    {
+
+        $result = DB::table('pedidos')
+            ->join('usuarios', 'pedidos.usuario_id', '=', 'usuarios.id') //Se genera el JOIN entre las dos tablas
+            ->select(
+                'usuarios.nombre as nombre_usuario',
+                'pedidos.producto',
+                DB::raw('SUM(pedidos.cantidad) as total_cantidad'), 
+                DB::raw('SUM(pedidos.total) as total_pedido')
+            )
+            ->groupBy('usuarios.id', 'usuarios.nombre', 'pedidos.producto') //Se usa 'groupBy' para agrupar las selecciones de la consulta
+            ->get();
+
+        return response()->json([
+            'Message' => 'Data selected Succesfully',
+            'data' => $result
+        ]);
+    }
 }
--- 02_Query_Builder/routes/api.php
+++ 02_Query_Builder/routes/api.php
@@ -8,7 +8,17 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('usuarios_ordenes', [QbuilderController::class, 'index']);
-Route::post('usuarios', [QbuilderController::class, 'insertUsuarios']);
-Route::post('pedidos', [QbuilderController::class, 'insertPedidos']);
-Route::delete('usuarios_ordenes', [QbuilderController::class, 'delete']);
+Route::get('usuarios_ordenes', [QbuilderController::class, 'index']);       //Ruta para obtener todos los registros
+Route::delete('usuarios_ordenes', [QbuilderController::class, 'delete']);   //Ruta para eliminar todos los registros
+
+Route::post('usuarios', [QbuilderController::class, 'insertUsuarios']);     //(Ejercicio #1) Ruta para agregar registros de los usuarios
+Route::post('pedidos', [QbuilderController::class, 'insertPedidos']);       //(Ejercicio #1) Ruta para agregar registros de los pedidos  
+Route::get('pedidos_02', [QbuilderController::class, 'getUser2']);          //(Ejercicio #2) Ruta obtener pedidos de usuario con ID 2    
+Route::get('detalle', [QbuilderController::class, 'detailOrder']);          //(Ejercicio #3) Ruta para obtener pedidos con nombre e email de usuario 
+Route::get('total_range', [QbuilderController::class, 'rangeOrders']);      //(Ejercicio #4) Ruta para obtener pedidos en un rango de $100 y $250
+Route::get('R_users', [QbuilderController::class, 'user_with_r']);          //(Ejercicio #5) Ruta para filtrar usuarios que inician con R
+Route::get('user_5', [QbuilderController::class, 'totalUser5']);            //(Ejercicio #6) Ruta para contar los pedidos del usuario con ID 5
+Route::get('descOrders', [QbuilderController::class, 'descOrders']);        //(Ejercicio #7) Ruta para filtrar pedidos con sus usuarios en orden DESC
+Route::get('total', [QbuilderController::class, 'sumTotal']);               //(Ejercicio #8) Ruta para obtener total de dinero de los pedidos
+Route::get('cheapest_total', [QbuilderController::class, 'minTotal']);      //(Ejercicio #9) Ruta para obtener el pedido mas economico con el nombre de usuario asociadp
+Route::get('filtro_pedidos', [QbuilderController::class, 'orders']);        //(Ejercicio #10) Ruta para obtener el producto, la cantidad y el total de cada pedido, agrupándolos por usuario
